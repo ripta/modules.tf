0b567a5dd2549e177e29b05d5f2d2bb718aacd60
Ripta Pasay <ripta@users.noreply.github.com>
2016-04-01 13:04:03 -0700

    feat(aws/vpc/natted): standardize exposed names

--- aws/vpc/natted/outputs.tf
+++ aws/vpc/natted/outputs.tf
@@ -6,8 +6,8 @@ output "public_subnet"  { value = "${aws_subnet.public.id}"  }
 output "private_route_id" { value = "${aws_route_table.private.id}" }
 output "public_route_id"  { value = "${aws_route_table.public.id}"  }
 
-output "sg_ingress" { value = "${aws_security_group.bastion.id}" }
-output "sg_egress"  { value = "${aws_security_group.nat.id}"     }
+output "ingress_sg_id" { value = "${aws_security_group.bastion.id}" }
+output "egress_sg_id"  { value = "${aws_security_group.nat.id}"     }
 
-output "bastion_public_ip" { value = "${aws_eip.bastion.public_ip}" }
-output "nat_public_ip"     { value = "${aws_eip.nat.public_ip}"     }
+output "ingress_public_ip" { value = "${aws_eip.bastion.public_ip}" }
+output "egress_public_ip"  { value = "${aws_eip.nat.public_ip}"     }
