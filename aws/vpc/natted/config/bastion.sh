ece6d305ef585029b8ab168847faec24c2b4fce4
Ripta Pasay <ripta@users.noreply.github.com>
2016-04-01 16:53:38 -0700

    docs(aws/vpc/natted): remove apt-get, add docs to config scripts

--- aws/vpc/natted/config/bastion.sh
+++ aws/vpc/natted/config/bastion.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-DEBIAN_FRONTEND=noninteractive apt-get update
+# bastion.sh - Prepare an EC2 instance as a bastion host
+#
+# This script changes the default SSH port from 22 to 2804.
 
 sed -i 's/^Port.*/Port 2804/g' /etc/ssh/sshd_config
 service ssh restart
--- aws/vpc/natted/config/nat.sh
+++ aws/vpc/natted/config/nat.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-DEBIAN_FRONTEND=noninteractive apt-get update
+# nat.sh - Prepare an EC2 instance as a NAT
+#
+# This script enables kernel IP forwarding for both IPv4 and IPv6. When combined
+# with an instance that has its `source_dest_check` set to `false`, any incoming
+# packet from ${vpc_cidr} will be forwarded to `eth0`.
 
 sed -i 's/^#net.ipv4.ip_forward.*/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
 sed -i 's/^#net.ipv6.conf.all.forwarding.*/net.ipv6.conf.all.forwarding = 1/g' /etc/sysctl.conf
