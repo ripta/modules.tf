388c01ecde30fe7febbeab84619935a1e37b2f75
Ripta Pasay <ripta@users.noreply.github.com>
2016-04-01 15:58:02 -0700

    feat(aws/vpc/natted): add "type" to variables

--- aws/vpc/natted/inputs.tf
+++ aws/vpc/natted/inputs.tf
@@ -1,32 +1,41 @@
 variable "az"   {
   description = "The qualified name of the AZ, e.g., us-west-2a"
+  type = "string"
   default = "us-west-2a"
 }
 
-variable "name" { description = "A descriptive name for the VPC." }
+variable "name" {
+  description = "A descriptive name for the VPC."
+  type = "string"
+}
 
 variable "cidr" {
   description = "The IP space for the VPC in CIDR form."
+  type = "string"
   default = "172.28.0.0/16"
 }
 
 variable "private_cidr" {
   description = "The IP space for the private subnet; must be contained in the VPC IP space (`cidr` above)."
+  type = "string"
   default = "172.28.16.0/20"
 }
 variable "public_cidr" {
   description = "The IP space for the public subnet; must be contained in the VPC IP space (`cidr` above)."
+  type = "string"
   default = "172.28.0.0/20"
 }
 
 variable "key_name" {
   description = "The name of the key-pair used for NAT and bastion instances."
+  type = "string"
 }
 
 variable "amis" {
-  description = "List of AMIs for each instance type"
+  description = "AMIs for each EC2 instance"
 
   # Debian 8 Jessie HVM (2015-06-07-12-27)
+  type = "map"
   default = {
     ingress = "ami-818eb7b1"
     egress = "ami-818eb7b1"
@@ -34,6 +43,8 @@ variable "amis" {
 }
 
 variable "instance_types" {
+  description = "Instance types for each EC2 instance"
+  type = "map"
   default = {
     ingress = "t2.micro"
     egress = "t2.small"
@@ -42,9 +53,11 @@ variable "instance_types" {
 
 variable "ingress_whitelist_ips" {
   description = "Comma-delimited (no spaces) list of external IPs allowed to SSH in"
+  type = "string"
 }
 
 variable "ingress_ssh_port" {
   description = "The port on `ingress_public_ip` to listen to for SSH"
+  type = "string"
   default = "2804"
 }
