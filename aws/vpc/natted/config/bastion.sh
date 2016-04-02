25f2e6b22bc1fc403dd506d5b3a482ce71ca2dfe
Ripta Pasay <ripta@users.noreply.github.com>
2016-04-02 11:22:16 -0700

    feat(aws/vpc/natted): add support for multiple pkg managers

--- aws/vpc/natted/config/bastion.sh
+++ aws/vpc/natted/config/bastion.sh
@@ -4,5 +4,17 @@
 #
 # This script changes the default SSH port from 22 to 2804.
 
+if [ -n "$(command -v yum)" ]
+then
+  yum update -y
+elif [ -n "$(command -v apt-get)" ]
+then
+  DEBIAN_FRONTEND=noninteractive apt-get update
+elif [ -n "$(command -v apk)" ]
+  apk update
+else
+  echo "Warning: could not find a supported package manager" >&2
+fi
+
 sed -i 's/^Port.*/Port 2804/g' /etc/ssh/sshd_config
 service ssh restart
--- aws/vpc/natted/config/nat.sh
+++ aws/vpc/natted/config/nat.sh
@@ -6,6 +6,18 @@
 # with an instance that has its `source_dest_check` set to `false`, any incoming
 # packet from ${vpc_cidr} will be forwarded to `eth0`.
 
+if [ -n "$(command -v yum)" ]
+then
+  yum update -y
+elif [ -n "$(command -v apt-get)" ]
+then
+  DEBIAN_FRONTEND=noninteractive apt-get update
+elif [ -n "$(command -v apk)" ]
+  apk update
+else
+  echo "Warning: could not find a supported package manager" >&2
+fi
+
 sed -i 's/^#net.ipv4.ip_forward.*/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
 sed -i 's/^#net.ipv6.conf.all.forwarding.*/net.ipv6.conf.all.forwarding = 1/g' /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
