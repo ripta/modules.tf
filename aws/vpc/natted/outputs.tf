d2b145e633fa3fb955e6101af319e79c122debc9
Ripta Pasay <ripta@users.noreply.github.com>
2016-04-04 10:47:20 -0700

    feat(aws/vpc/natted): expose private IPs

--- aws/vpc/natted/outputs.tf
+++ aws/vpc/natted/outputs.tf
@@ -9,5 +9,8 @@ output "public_route_id"  { value = "${aws_route_table.public.id}"  }
 output "ingress_sg_id" { value = "${aws_security_group.bastion.id}" }
 output "egress_sg_id"  { value = "${aws_security_group.nat.id}"     }
 
+output "bastion_private_ip" { value = "${aws_instance.bastion.private_ip}" }
+output "nat_private_ip"     { value = "${aws_instance.nat.private_ip}"     }
+
 output "ingress_public_ip" { value = "${aws_eip.bastion.public_ip}" }
 output "egress_public_ip"  { value = "${aws_eip.nat.public_ip}"     }
