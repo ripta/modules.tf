4e01ea2e6a0a26283f843373b535326a987ea06a
Ripta Pasay <ripta@users.noreply.github.com>
2016-10-29 02:23:15 -0700

    feat(aws/vpc/natted): export NAT instance IDs

--- aws/vpc/natted/outputs.tf
+++ aws/vpc/natted/outputs.tf
@@ -38,6 +38,10 @@ output "nat_private_ip" {
   value = ["${aws_instance.nat.*.private_ip}"]
 }
 
+output "nat_instances" {
+  value = ["${aws_instance.nat.*.id}"]
+}
+
 output "ingress_public_ip" {
   value = ["${aws_eip.bastion.*.public_ip}"]
 }
